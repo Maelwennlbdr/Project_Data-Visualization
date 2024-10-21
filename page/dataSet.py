f638412a90a4b8dad422fdaefd90ee34a7c5999b
Maelwenn <maelwenn@labidurie.fr>
2024-10-21 15:21:52 +0200

    english columns

--- page/dataSet.py
+++ page/dataSet.py
@@ -73,17 +73,47 @@ file_path = './new/dataset_frequence_cine.xlsx'
 
 # Chargement et nettoyage de la feuille 'mois'
 df_mois_clean = pd.read_excel(file_path, sheet_name='mois', skiprows=6, nrows=45)
-df_mois_clean.rename(columns={df_mois_clean.columns[0]: 'Years'}, inplace=True)
+df_mois_clean.rename(columns={
+    df_mois_clean.columns[0]: 'Years',
+    df_mois_clean.columns[1]: 'January',
+    df_mois_clean.columns[2]: 'February',
+    df_mois_clean.columns[3]: 'March',
+    df_mois_clean.columns[4]: 'April',
+    df_mois_clean.columns[5]: 'May',
+    df_mois_clean.columns[6]: 'June',
+    df_mois_clean.columns[7]: 'July',
+    df_mois_clean.columns[8]: 'August',
+    df_mois_clean.columns[9]: 'September',
+    df_mois_clean.columns[10]: 'October',
+    df_mois_clean.columns[11]: 'November',
+    df_mois_clean.columns[12]: 'December',
+    df_mois_clean.columns[13]: 'Total'
+}, inplace=True)
 
 # Chargement et nettoyage de la feuille 'freqciné'
 df_freqcine_clean = pd.read_excel(file_path, sheet_name='freqciné', skiprows=50, nrows=45)
-df_freqcine_clean = pd.read_excel(file_path, sheet_name='freqciné', skiprows=50, nrows=45)
-df_freqcine_clean.rename(columns={df_freqcine_clean.columns[0]: 'Years'}, inplace=True)
+df_freqcine_clean.rename(columns={
+    df_freqcine_clean.columns[0]: 'Years',
+    df_freqcine_clean.columns[1]: 'Sessions (thousands)',
+    df_freqcine_clean.columns[2]: 'Entries (millions)',
+    df_freqcine_clean.columns[3]: 'Box office (M€ current)',
+    df_freqcine_clean.columns[4]: 'Average revenue per entry (€)'
+}, inplace=True)
 df_freqcine_clean.replace('-', np.nan, inplace=True)
 
 # Chargement et nettoyage de la feuille 'entrées ff'
 df_entrees_ff_clean = pd.read_excel(file_path, sheet_name='entrées ff', skiprows=6, nrows=33)
-df_entrees_ff_clean.rename(columns={df_entrees_ff_clean.columns[0]: 'Years'}, inplace=True)
+df_entrees_ff_clean.rename(columns={
+    df_entrees_ff_clean.columns[0]: 'Years',
+    df_entrees_ff_clean.columns[1]: 'More than 2 million entries',
+    df_entrees_ff_clean.columns[2]: '1 to 2 million entries',
+    df_entrees_ff_clean.columns[3]: '500,000 to 1 million entries',
+    df_entrees_ff_clean.columns[4]: '200,000 to 500,000 entries',
+    df_entrees_ff_clean.columns[5]: '100,000 to 200,000 entries',
+    df_entrees_ff_clean.columns[6]: '50,000 to 100,000 entries',
+    df_entrees_ff_clean.columns[7]: 'Less than 50,000 entries',
+    df_entrees_ff_clean.columns[8]: 'Total'
+}, inplace=True)
 
 # Convert the image to base64
 image_path = "file/cinema.webp"
@@ -117,11 +147,11 @@ custom_colors = ['#F7EDE2', '#F5CAC3', '#F28482']
 
 fig = px.scatter(df_freqcine_clean,
                  x='Years',
-                 y='Recette guichet (M€ courants)',
-                 color='Recette moyenne par entrée (€)',
+                 y='Box office (M€ current)',
+                 color='Average revenue per entry (€)',
                  labels={
                      'Years': 'Years',
-                     'Recette guichet (M€ courants)': 'Receipt counter (current M€)',
+                     'Box office (M€ current)': 'Receipt counter (current M€)',
                      'color': 'Average revenue per entry (€)'
                  },
                  color_continuous_scale=custom_colors)
@@ -136,15 +166,14 @@ filtered_data = df_mois_clean[df_mois_clean['Years'].isin(years_of_interest)]
 
 filtered_data = filtered_data.replace('-', pd.NA).astype(float)
 
-months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
-#months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 scatter_data = pd.melt(
     filtered_data,
     id_vars=['Years'],
     value_vars=months,
-    var_name='Mois',
-    value_name='Moyenne Mensuelle'
+    var_name='Months',
+    value_name='Monthly average'
 )
 
 custom_colors = ['#F28482', '#84A59D', '#F6BD60', '#C7B8A8', '#F5CAC3', '#C2837A']
@@ -152,12 +181,12 @@ custom_colors = ['#F28482', '#84A59D', '#F6BD60', '#C7B8A8', '#F5CAC3', '#C2837A
 st.markdown('## **Monthly averages attendance of decade**')
 fig = px.line(
     scatter_data,
-    x='Mois',
-    y='Moyenne Mensuelle',
+    x='Months',
+    y='Monthly average',
     color='Years',
     markers=True,
     color_discrete_sequence=custom_colors,
-    labels={'Moyenne Mensuelle': 'Moyenne Mensuelle', 'Mois': 'Mois'}
+    labels={'Monthly average': 'Monthly average', 'Months': 'Months'}
 )
 st.plotly_chart(fig)
 
@@ -166,12 +195,27 @@ st.markdown('## **Income by Years**')
 tab1, tab2 = st.tabs(["Total", "Average"])
 
 df_mois_revenu = pd.read_excel(file_path, sheet_name='mois', skiprows=53, nrows=45)
-df_mois_revenu.rename(columns={df_mois_revenu.columns[0]: 'Years'}, inplace=True)
+df_mois_revenu.rename(columns={
+    df_mois_revenu.columns[0]: 'Years',
+    df_mois_revenu.columns[1]: 'January',
+    df_mois_revenu.columns[2]: 'February',
+    df_mois_revenu.columns[3]: 'March',
+    df_mois_revenu.columns[4]: 'April',
+    df_mois_revenu.columns[5]: 'May',
+    df_mois_revenu.columns[6]: 'June',
+    df_mois_revenu.columns[7]: 'July',
+    df_mois_revenu.columns[8]: 'August',
+    df_mois_revenu.columns[9]: 'September',
+    df_mois_revenu.columns[10]: 'October',
+    df_mois_revenu.columns[11]: 'November',
+    df_mois_revenu.columns[12]: 'December',
+    df_mois_revenu.columns[13]: 'Total'
+}, inplace=True)
 
 
 months = df_mois_revenu.columns[1:-1]
 df_melted = df_mois_revenu.melt(id_vars=['Years'], value_vars=months,
-                                  var_name='Month', value_name='Revenue')
+                                  var_name='Months', value_name='Income')
 
 average_revenue_per_year = df_mois_revenu.drop(columns=['Total']).mean(axis=1)
 df_mois_revenu['Average income'] = average_revenue_per_year
@@ -179,7 +223,7 @@ df_mois_revenu['Average income'] = average_revenue_per_year
 average_revenue_by_year = df_mois_revenu[['Years', 'Average income']]
 # Tab 1: Display data by Years
 tab1.subheader('Total income by Years')
-fig_years = px.bar(df_freqcine_clean, x='Years', y='Recette guichet (M€ courants)')
+fig_years = px.bar(df_freqcine_clean, x='Years', y='Box office (M€ current)')
 fig_years.update_traces(marker_color='#F6BD60')
 tab1.plotly_chart(fig_years)
 
@@ -190,42 +234,31 @@ fig_months = px.bar(average_revenue_by_year, x='Years', y='Average income', labe
 fig_months.update_traces(marker_color='#F6BD60')
 tab2.plotly_chart(fig_months)
 
-
 st.markdown('## **Histogram of total admissions in cinema by year for French films**')
 
-# Prepare data for bar chart
 bar_chart_data = df_entrees_ff_clean.set_index('Years')
+st.bar_chart(bar_chart_data['Total'], color='#84A59D')
 
-# Create the bar chart
-st.bar_chart(bar_chart_data['total'], color='#84A59D')
-
-
-
-st.markdown('## **Number of Movies by entries by Year**')
-# Create tabs for each year
+st.markdown('## **Number of Movies by Entries by Year**')
 years = [1995, 2000, 2005, 2010, 2015, 2020, 2023]
-tabs = st.tabs([str(year) for year in years])  # Create tabs for the years
+tabs = st.tabs([str(year) for year in years])
 
-# Define custom colors
 custom_colors = ['#F28482', '#3B524C', '#84A59D', '#F6BD60', '#C7B8A8', '#F5CAC3', '#C2837A']
 
 for i, year in enumerate(years):
-    # Filter data for the specific year
     year_data = df_entrees_ff_clean[df_entrees_ff_clean['Years'] == year]
 
-    # Melt the DataFrame to prepare for plotting
     melted_data = year_data.melt(id_vars=['Years'],
-                                  value_vars=[col for col in df_entrees_ff_clean.columns if col != 'Years' and col != 'total'],
+                                  value_vars=[col for col in df_entrees_ff_clean.columns if col != 'Years' and col != 'Total'],
                                   var_name='Entry Category',
                                   value_name='Number of Entries')
 
-    # Create the bar chart with custom colors
     fig = px.bar(melted_data,
                  x='Entry Category',
                  y='Number of Entries',
                  labels={'Number of Entries': 'Number of Movies', 'Entry Category': 'Entry Category'},
                  color='Entry Category',
-                 color_discrete_sequence=custom_colors)  # Use the custom color palette
+                 color_discrete_sequence=custom_colors)
 
     tabs[i].subheader(year)
-    tabs[i].plotly_chart(fig)  # Use the index to access the correct tab
+    tabs[i].plotly_chart(fig)
